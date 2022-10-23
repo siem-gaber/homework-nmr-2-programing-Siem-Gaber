70e6ceb703611b40917478b4f1a717bbab6bbdfe
siem-gaber <116160960+siem-gaber@users.noreply.github.com>
2022-10-23 13:17:45 +0200

    Rename Main.c to Main(2).c

--- Main(2).c
+++ Main(2).c
@@ -1,13 +1,13 @@
 /*******************************************************************************
-* 2.2.c: R‰ttning av felkod. I programmet skall Kalle Anks namn, Âlder och
-*        syssels‰ttning (serietidningsfigur) skrivas ut i terminalen.
-*        Programmet kompilerar dock inte pÂ grund av ett antal fel, som
-*        d‰rmed mÂste korrigeras.
+* 2.2.c: R√§ttning av felkod. I programmet skall Kalle Anks namn, √•lder och
+*        syssels√§ttning (serietidningsfigur) skrivas ut i terminalen.
+*        Programmet kompilerar dock inte p√• grund av ett antal fel, som
+*        d√§rmed m√•ste korrigeras.
 *******************************************************************************/
 #include <stdio.h>
 
 /*******************************************************************************
-* person_print: Skriver ut personuppgifter i form av namn, Âlder samt arbete.
+* person_print: Skriver ut personuppgifter i form av namn, √•lder samt arbete.
 *******************************************************************************/
 static void* person_print(const char* name, const char *age, const char* occupation)
 {
